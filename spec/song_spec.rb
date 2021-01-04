79b77944f550e0bb6daab0001baae467663eed4b
Christopher Spagnesi <christopherspagnesi@gmail.com>
2021-01-04 19:10:03 +0000

    Automatically backed up by Learn

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,36 +1,21 @@
-require 'pry'
+class Song
+  attr_accessor :name, :artist
 
-describe 'Song' do
-  let(:song) {Song.new('Man in the Mirror')}
-  let(:file_name) {'Michael Jackson - Black or White - pop.mp3'}
-
-  describe '#initialize with #name' do
-    it 'accepts a name for the song' do
-      expect(song.name).to eq('Man in the Mirror')
-    end
+  def initialize(name)
+    @name = name
   end
 
-  describe '#name=' do
-    it 'sets the song name' do
-      song.name = 'Thriller'
-      expect(song.name).to eq('Thriller')
-    end
+  def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
   end
 
-  describe '#artist=' do
-    it 'sets the artist object to belong to the song' do
-      new_artist_object = Artist.new('King of Pop')
-      song.artist = new_artist_object
-      expect(song.artist).to eq(new_artist_object)
-    end
-  end
-
-  describe '.new_by_filename' do
-    it 'creates a new instance of a song from the file that\'s passed in' do
-      #binding.pry
-      new_instance = Song.new_by_filename(file_name)
-      expect(new_instance.name).to eq('Black or White')
-      expect(new_instance.artist.name).to eq('Michael Jackson')
-    end
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
   end
 end
